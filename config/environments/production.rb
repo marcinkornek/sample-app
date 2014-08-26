50c4a9d64dad6d08c75754ed692393d4efec30f0
marcinkornek <marcinkornek@gmail.com>
2014-08-26 21:55:49 +0200

    trying to fix heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,7 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
+  config.action_mailer.default_url_options = { :host => 'stark-temple-3029.herokuapp.com' }
+
 
 end
